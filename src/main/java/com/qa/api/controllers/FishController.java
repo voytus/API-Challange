f8bce665cd8982f93bd3ea55531060c46514b784
voytus <voytus@protonmail.com>
2022-06-07 11:02:26 +0100

    WTM: added app-prod.properties

--- src/main/java/com/qa/api/controllers/FishController.java
+++ src/main/java/com/qa/api/controllers/FishController.java
@@ -1,5 +1,8 @@
 package com.qa.api.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.api.services.FishService;
@@ -12,4 +15,6 @@ public class FishController {
 	public FishController(FishService service) {
 		this.service = service;
 	}
+	
+	private List<Fish> fishes = new ArrayList<>();
 }
